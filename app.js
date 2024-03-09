096e6bd0fc92d8f2ebf5bf6543fc2d7e8ffc9092
Mauritz Marais <mauritz303@gmail.com>
2024-03-09 10:06:58 +0200

    added env variables

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
 const routes = require("./routes/index");
 
+app.use(express.json());
 // routes
 app.use('/', routes);
 
