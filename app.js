2e663413fa2287fe923a611def00a317ebec9e72
Mauritz Marais <mauritz303@gmail.com>
2024-03-26 16:23:33 +0200

    updated cors options

--- app.js
+++ app.js
@@ -2,8 +2,24 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 const routes = require("./routes/index");
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-app.use(express.json());
+ const corsOptions = {
+     origin: 'http://localhost:5173',
+     optionsSuccessStatus: 200
+ }
+
+app.use(cors(corsOptions));
+
+// Enable the use of request body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+// app.use(express.json());
+
 // routes
 app.use('/', routes);
 
