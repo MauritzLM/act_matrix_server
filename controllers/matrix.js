88fcb5edd725db2de68a51260fb2a387420ab7e2
Mauritz Marais <mauritz303@gmail.com>
2024-03-27 16:38:22 +0200

    added validation to create new function

--- controllers/matrix.js
+++ controllers/matrix.js
@@ -11,6 +11,7 @@ exports.getMatrix = [auth,
             // get instance id from request
             const { id } = req.body;
 
+
             // get matrix using title
             const text = 'SELECT * FROM matrix_instances WHERE instance_id = $1';
             const values = [id];
@@ -34,16 +35,16 @@ exports.getAllMatrix = [auth,
     async function (req, res, next) {
         try {
             // get user id from request
-            const { id } = req.body;
+            const { user_id } = req.body;
 
             // query db
-            const text = 'SELECT * FROM matrix_instances WHERE user_profile = $2';
-            const values = [id];
+            const text = 'SELECT * FROM matrix_instances WHERE user_profile = $1';
+            const values = [user_id];
 
             const result = await db.query(text, values);
 
             // return result
-            const user_matrix_instances = result.rows[0];
+            const user_matrix_instances = result.rows;
 
             res.json(user_matrix_instances);
 
@@ -57,11 +58,24 @@ exports.getAllMatrix = [auth,
 // create new matrix
 exports.createMatrix = [
     auth,
+    body('title', 'please enter a title')
+        .trim()
+        .notEmpty()
+        .isLength({ min: 4, max: 20 })
+        .escape(),
     async function (req, res, next) {
         try {
+
+            // validation errors
+            const errors = validationResult(req);
+
+            if (!errors.isEmpty()) {
+                res.json({ errors: errors.array() });
+                return;
+            }
+            
             // get title and user id from request
-            const { title } = req.body;
-            const { user_id } = req.user;
+            const { title, user_id } = req.body;
 
             // create new matrix instance
             const text = 'INSERT INTO matrix_instances (user_profile, title) VALUES ($1, $2) RETURNING instance_id';
@@ -76,11 +90,11 @@ exports.createMatrix = [
                 return res.json("something went wrong")
             };
 
-            // if successful add instance to user_profile
-            const user_text = 'UPDATE user_profiles SET matrix_instances = array_append(matrix_instances, $1) WHERE user_id = $2';
-            const user_values = [instance_id, user_id];
+            // if successful add instance to user_profile (not needed anymore*)
+            // const user_text = 'UPDATE user_profiles SET matrix_instances = array_append(matrix_instances, $1) WHERE user_id = $2';
+            // const user_values = [instance_id, user_id];
 
-            const user_result = await db.query(user_text, user_values);
+            // const user_result = await db.query(user_text, user_values);
 
             // send success message
             res.json({ msg: 'matrix created' });
@@ -109,7 +123,7 @@ exports.updateMatrix = [
 
             // query db
             const text = 'UPDATE matrix_instances SET quadrant_1 = $1, quadrant_2 = $2, quadrant_3 = $3, quadrant_4 = $4 WHERE instance_id = $5';
-            const values = [q1_sanitized, q2_sanitized, q3_sanitized, q4_sanitized , instance_id];
+            const values = [q1_sanitized, q2_sanitized, q3_sanitized, q4_sanitized, instance_id];
 
             const result = await db.query(text, values);
 
